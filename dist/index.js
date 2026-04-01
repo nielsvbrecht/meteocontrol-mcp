2b08d881fc14f1477321adf7eb73aeef4d90980e
Niels van Brecht <github@vanbrecht.family>
2026-04-01 13:54:08 +0200

    chore: Remove build and coverage artifacts from git tracking

--- dist/index.js
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env node
-import { MeteoControlServer } from './server.js';
-const server = new MeteoControlServer();
-server.run().catch((error) => {
-    console.error('Fatal error running server:', error);
-    process.exit(1);
-});
-//# sourceMappingURL=index.js.map
